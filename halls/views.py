7b6a92e92721cebb515a142047f1113ff94fb962
thunderboyk <lakshyajun27@gmail.com>
2024-10-01 23:06:47 +0530

    Add files via upload

--- halls/views.py
+++ halls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
 # Create your views here.
-
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request,'halls/home.html')
+
+class Signup(generic.CreateView):
+    form_class = UserCreationForm
+    sucess_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
--- hofvid/urls.py
+++ hofvid/urls.py
@@ -14,16 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path
 from halls import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
-
-urlpatterns = [
+urlpatterns =[
     path('admin/', admin.site.urls),
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    # AUTH
+    path('signup',views.Signup.as_view(), name ="signup"),
+    path('login',auth_views.LoginView.as_view(), name ="login"),
+    path('logout',auth_views.LogoutView.as_view(), name ="logout"),
 ]
 
 urlpatterns+= static(settings.STATIC_URL,documnet_root=settings.STATIC_ROOT)
